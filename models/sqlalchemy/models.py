972ff1799a533f0fec87d0f1d0620e66c0410b9e
braviss <doctype.developer@gmail.com>
2023-10-08 10:24:27 +0300

    -> upg models

--- models/sqlalchemy/models.py
+++ models/sqlalchemy/models.py
@@ -1,5 +1,5 @@
 from database import db
-
+from datetime import datetime
 
 class Animal(db.Model):
     __tablename__ = 'animal'
@@ -9,3 +9,10 @@ class Animal(db.Model):
     animal_type = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
     birth_date = db.Column(db.Date, nullable=False)
+
+
+    @property
+    def age(self):
+        today = datetime.now()
+        age = today.year - self.birth_date.year
+        return age
