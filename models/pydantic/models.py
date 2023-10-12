acac91056687ced3d422c49f7bc90fa327c0a351
user-sspmynxdvb <pewfellowmnemonic@gmail.com>
2023-10-12 19:38:57 +0300

    v2

--- models/pydantic/models.py
+++ models/pydantic/models.py
@@ -6,6 +6,8 @@ class AnimalCreate(BaseModel):
     animal_type: str
     name: str
     birth_date: date
+    animal_breed: str
+    image_url: str
 
 
 class AnimalResponse(BaseModel):
@@ -15,3 +17,30 @@ class AnimalResponse(BaseModel):
     animal_type: str
     name: str
     birth_date: date
+    animal_breed: str
+    image_url: str
+
+    def get_age(self) -> str:
+        """
+        Returns:
+            str
+        """        
+        today = date.today()
+        years = today.year - self.birth_date.year
+        months = today.month - self.birth_date.month
+
+        if today.day < self.birth_date.day:
+            months -= 1
+
+        if months < 0:
+            years -= 1
+            months += 12
+
+        if years > 0 and months > 0:
+            age_str = f"{years} year {months} month"
+        elif years > 0:
+            age_str = f"{years} year"
+        else:
+            age_str = f"{months} month"
+
+        return age_str
